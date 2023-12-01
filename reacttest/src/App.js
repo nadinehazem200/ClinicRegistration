a9c0de6bf3859e030f379995f07a1972a76a6026
bassma-khaled14 <bassmakhaleed123@gmail.com>
2023-12-01 04:06:53 +0200

    update app.js

--- reacttest/src/App.js
+++ reacttest/src/App.js
@@ -1,25 +1,46 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "../src/Components/SignIn/SignIn";
 import SignUp from "../src/Components/SignUp/SignUp";
-import ClinicReservation from "../src/Components/ClinicReservation/ClinicReservation";
-import DoctorSchedule from "../src/Components/DoctorSchedule/DoctorSchedule";
+import CancelAppointment from "./Components/PatientCancelApp/CancelAppointment";
 import Layout from "../src/Components/Layout/Layout";
-import Navbar from "../src/Components/Navbar/Navbar";
-
+import UpdateApp from "./Components/PatientUpdateApp/UpdateAPP";
+import PatientCreateApp from "./Components/PatientCreateApp/PatientCreateApp";
+import HomePage from "./Components/HomePage/HomePage";
+import PatientView from "./Components/PatientView/Patientview";
+import DrView from "./Components/DrView/DrView";
+import ListPatientApp from "./Components/ListPatientApp/ListPatientApp";
+import SelectDr from "./Components/PatientSelectSlot/SelectDr";
+import ListDrApp from "./Components/ListDrApp/ListDrApp";
+import DrInserSlot from "./Components/DrInsertSlot/DrInsertSlot";
+import ListAvailableSlots from "./Components/ListAvaiableSlots/ListAvaiableSlots";
 function App() {
+  
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="doctorschedule" element={<DoctorSchedule />} />
-        <Route path="clinicreservation" element={<ClinicReservation />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="patientview" element={<PatientView/>} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/drview" element={<DrView />} />
+        <Route path="/CancelAppointment" element={<CancelAppointment />} />
+        <Route path="/appointments/:appID" element={<UpdateApp />} />
+        <Route path="/createapp" element={<PatientCreateApp />} />
+        <Route path="/patients/:patientID" element={<ListPatientApp />} />
+        <Route path="/selectdr" element={<SelectDr />} />
+        <Route path="/ListDrApp" element={< ListDrApp />} />
+        <Route path="/DrInserSlot" element={<DrInserSlot />} />
+        <Route path="/doctors/available-slots" element={<ListAvailableSlots />} />
+
+
+
+
+
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
